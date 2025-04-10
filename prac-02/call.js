8b902ce60352e93f0a9ab2c8e900e68742d8b4fe
chengmingLi <ccaihong@icloud.com>
2025-04-10 17:54:05 +1000

    手撕call方法

--- prac-02/call.js
+++ prac-02/call.js
@@ -1,58 +1,39 @@
-// 共享传递， 传递的是地址的index
+// call 将调用他的函数的this绑定到传入的对象上，并且可以传入参数
+// call的实现
 
-// var obj = {
-//     value: 1
-// };
-
-// function foo(o) {
-//     o.value = 2;
-//     console.log(o); // 此时传入的obj是一份copy, 其value指向了heap里的地址
-// }
-// foo(obj)
-// vonsole.log(obj.value) // 原obj其value指向依然是1
-// // 参数如果是基本类型，按值传递
-// // 参数如果是引用类型，共享传递 
-
-// var obj = {
-//     value: 1
-//     };
-//     function foo(o) {
-//         o = 2;
-//         console.log(o); //2
-//     }
-//     foo(obj);
-//     console.log(obj.value) // 1 此时如果是o.value会undefined
-
-
-Function.prototype.call2 = function(context) {
-    var context = context || window;
-    context.fn= this // this指向了调用此函数的函数，将其与context进行绑定
-
-    let arg = [...arguments].slice(1)
-    let result = context.fn(...arg)
-    delete context.fn;
-
-    return result;
+// 首先我们有一个对象
+const foo = {
+    name: "lichengming",
+    goal:"我要手撕call方法！"
 }
 
-var value = 2; // 全局的2，这样传入null的时候，window就会抓这个2
-
-let foo = {
-    value: 1
-}
-
-function bar(name, age) {
-    console.log(name)
+const bar = function(age, height) {
+    console.log(this.name);
+    console.log(this.goal)
     console.log(age)
-    console.log(this.value)
+    console.log(height)
 }
 
-bar.call2(foo, "kevin", 12) // 此时bar.call2(foo) 等价于将 bar 的 this 绑定为 foo 后调用
-// bar.call2(null);
-console.log(bar.call2(foo, 'kevin', 18));
+Function.prototype.myCall = function(context) {
+    // 边界检查，没值就指向window
+    if (typeof context == 'undefined' || context === null) {
+        context = window;
+    }
 
-// 小总结，call函数的主要功能就是把函数指向外部调用的函数。
-// 需要fn做绑定
-// [...arguments]slice(1),接受多个参数
-// var context = context || window, 接受如果是null就去全局window找
-// let result = context.fn（...arg) 实现了返回
+    // 把调用的函数临时挂载在context身上
+    let fnSymbol = Symbol() //Symbol ES6新标准，更干净高效
+    context[fnSymbol] = this;
+    // arguments的第一位我们不要
+    let args = [...arguments].slice(1)
+    // 存起来return出去
+    result = context[fnSymbol](...args)
+
+    delete context[fnSymbol]
+    return result
+
+}
+
+console.log(bar.myCall(foo))
+console.log(bar.myCall(foo, 22))
+console.log(bar.myCall(foo, 22, 175))
+console.log(null)
