33c97b92be16476c7a836223442a5d0ee8f12ec1
XavierElco <ccaihong@icloud.com>
2025-05-24 00:51:52 +1000

    useSyncExternalStore

--- react/react-basic/react-demo/src/App.tsx
+++ react/react-basic/react-demo/src/App.tsx
@@ -1,21 +1,13 @@
-
-import Card from './components/Card'
-import Test from './components/Test'
-import Test2 from './components/Test2'
+import { useStorage } from './components/UseStorage/index'
 import './App.css'
 
-const fn = (params: string) => {
-  console.log("孩子组件传给父母组件", params)
-}
+
 function App() {
+  const [val, setVal] = useStorage('data', 1)
   return (
     <>
-
-      <Card callBack={fn} title="我是第一张卡片"></Card>
-      <Card title="我是第二张卡片"></Card>
-      <Card title="我是第三张卡片"></Card>
-      <Test></Test>
-      <Test2></Test2>
+      <h3>{val}</h3>
+      <button onClick={() => setVal(val + 1)}>设置val</button>
     </>
   )
 }
