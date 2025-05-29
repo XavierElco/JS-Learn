b0df6bdcbbb90b9be8be36d03191ae10ec960807
XavierElco <ccaihong@icloud.com>
2025-05-29 19:11:26 +1000

    useImperativeHandler

--- react/react-basic/react-demo/src/App.tsx
+++ react/react-basic/react-demo/src/App.tsx
@@ -1,17 +1,69 @@
-import React, { useLayoutEffect, useEffect, useRef } from 'react';
-import './App.css';
-function App() {
-  useLayoutEffect(() => {
-    const list = document.getElementById('list') as HTMLUListElement;
-    list.scrollTop = 900
-  }, [])
+import React, { useRef, useState, useImperativeHandle } from 'react';
 
+interface ChildRef {
+  name: string
+  validate: () => string | true
+  reset: () => void
+}
+
+const Child = ({ ref }: { ref: React.Ref<ChildRef> }) => {
+  const [form, setForm] = useState({
+    username: '',
+    password: '',
+    email: ''
+  })
+  const validate = () => {
+    if (!form.username) {
+      return '用户名不能为空'
+    }
+    if (!form.password) {
+      return '密码不能为空'
+    }
+    if (!form.email) {
+      return '邮箱不能为空'
+    }
+    return true
+  }
+  const reset = () => {
+    setForm({
+      username: '',
+      password: '',
+      email: ''
+    })
+  }
+  useImperativeHandle(ref, () => {
+    return {
+      name: 'child',
+      validate: validate,
+      reset: reset
+    }
+  })
+  return <div style={{ marginTop: '20px' }}>
+    <h3>我是表单组件</h3>
+    <input value={form.username} onChange={(e) => setForm({ ...form, username: e.target.value })} placeholder='请输入用户名' type="text" />
+    <input value={form.password} onChange={(e) => setForm({ ...form, password: e.target.value })} placeholder='请输入密码' type="text" />
+    <input value={form.email} onChange={(e) => setForm({ ...form, email: e.target.value })} placeholder='请输入邮箱' type="text" />
+  </div>
+}
+
+function App() {
+  const childRef = useRef<ChildRef>(null)
+  const showRefInfo = () => {
+    console.log(childRef.current)
+  }
+  const submit = () => {
+    const res = childRef.current?.validate()
+    console.log(res)
+  }
   return (
-    <ul id='list' style={{height: '500px', overflowY:'scroll'}}>
-      {Array.from({ length:500 }, (_, i) => (
-        <li key={i}>Item {i + 1}</li>
-      ))}
-    </ul>
+    <div>
+      <h2>我是父组件</h2>
+      <button onClick={showRefInfo}>获取子组件信息</button>
+      <button onClick={() => submit()}>校验子组件</button>
+      <button onClick={() => childRef.current?.reset()}>重置</button>
+      <hr />
+      <Child ref={childRef}></Child>
+    </div>
   );
 }
 
