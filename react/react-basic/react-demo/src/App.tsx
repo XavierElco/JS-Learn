ee3b209b4b3579a38be042c8e75b676d0ca00b6c
XavierElco <ccaihong@icloud.com>
2025-05-29 16:28:58 +1000

    useLayoutEffect

--- react/react-basic/react-demo/src/App.tsx
+++ react/react-basic/react-demo/src/App.tsx
@@ -1,72 +1,18 @@
-import { useEffect, useState } from "react"
-// 子组件
-
-// 清理函数的核心作用是确保组件卸载时能够正确清理副作用，避免：
-// 内存泄漏
-// 不必要的网络请求
-// 事件监听器累积
-// 对已卸载组件的状态更新
-// 资源浪费
-
-interface UserData {
-  name: string;
-  email: string;
-  username: string;
-  phone: string;
-  website: string;
-}
-
-
-const App = () => {
-  const [userID, setUserID] = useState(1)
-  const [userData, setUserData] = useState<UserData | null>(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    const fetchUserData = async() => {
-      try{
-        const response = await fetch(`https://jsonplaceholder.typicode.com/users/${userID}`)
-        if (!response.ok) {
-          throw new Error ('网络响应错误')
-        }
-        const data = await response.json()
-        setUserData(data)
-        setLoading(false)
-      } catch (err: any) {
-        setError(err.message);
-      } finally {
-        setLoading(false)
-      }
-    }
-    fetchUserData();
-  }, [userID])
-
-  const handleUserChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setUserID(parseInt(event.target.value))
-  }
+import React, { useLayoutEffect, useEffect, useRef } from 'react';
+import './App.css';
+function App() {
+  useLayoutEffect(() => {
+    const list = document.getElementById('list') as HTMLUListElement;
+    list.scrollTop = 900
+  }, [])
 
   return (
-    <div>
-      <h1>用户信息应用</h1>
-      <label>
-        输入用户ID:
-        <input type="number" value={userID} onChange={handleUserChange} min="1" max="10" />
-      </label>
-      {loading && <p>加载中...</p>}
-      {error && <p>错误: {error}</p>}
-      {userData && (
-      <div>
-        <h2>用户信息</h2>
-        <p>姓名: {userData.name}</p>
-        <p>邮箱: {userData.email}</p>
-        <p>用户名: {userData.username}</p>
-        <p>电话: {userData.phone}</p>
-        <p>网站: {userData.website}</p>
-      </div>
-    )}
-    </div>
+    <ul id='list' style={{height: '500px', overflowY:'scroll'}}>
+      {Array.from({ length:500 }, (_, i) => (
+        <li key={i}>Item {i + 1}</li>
+      ))}
+    </ul>
   );
 }
 
-export default App
+export default App;
