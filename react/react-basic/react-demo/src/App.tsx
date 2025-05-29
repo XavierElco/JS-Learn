837c79c661ba4fd0fcc57766a9f403a8b3efee2a
XavierElco <ccaihong@icloud.com>
2025-05-29 19:27:12 +1000

    useMemo

--- react/react-basic/react-demo/src/App.tsx
+++ react/react-basic/react-demo/src/App.tsx
@@ -1,54 +1,49 @@
-import React, { useContext, useState } from 'react';
-const ThemeContext = React.createContext<ThemeContextType>({} as ThemeContextType);
-interface ThemeContextType {
-  theme: string;
-  setTheme: (theme: string) => void;
-}
+import React, { useMemo, useState } from 'react';
 
-const Child = () => {
-  const themeContext = useContext(ThemeContext);
-  const styles = {
-    backgroundColor: themeContext.theme === 'light' ? 'white' : 'black',
-    border: '1px solid red',
-    width: 100 + 'px',
-    height: 100 + 'px',
-    color: themeContext.theme === 'light' ? 'black' : 'white'
-  }
-  return <div>
-    <div style={styles}>
-      child
-    </div>
-  </div>
-}
-
-const Parent = () => {
-  const themeContext = useContext(ThemeContext);
-  const styles = {
-    backgroundColor: themeContext.theme === 'light' ? 'white' : 'black',
-    border: '1px solid red',
-    width: 100 + 'px',
-    height: 100 + 'px',
-    color: themeContext.theme === 'light' ? 'black' : 'white'
-  }
-  return <div>
-    <div style={styles}>
-      Parent
-    </div>
-    <Child />
-  </div>
-}
 function App() {
-  const [theme, setTheme] = useState('light');
+  const [search, setSearch] = useState('');
+  const [goods, setGoods] = useState([
+    { id: 1, name: '苹果', price: 10, count: 1 },
+    { id: 2, name: '香蕉', price: 20, count: 1 },
+    { id: 3, name: '橘子', price: 30, count: 1 },
+  ]);
+  const handleAdd = (id: number) => {
+    setGoods(goods.map(item => item.id === id ? { ...item, count: item.count + 1 } : item));
+  }
+  const handleSub = (id: number) => {
+    setGoods(goods.map(item => item.id === id ? { ...item, count: item.count - 1 } : item));
+  }
+  const total = useMemo(() => {
+    console.log('total');
+    return  goods.reduce((total, item) => total + item.price * item.count, 0)
+  }, [goods]);
   return (
     <div>
-      <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>切换主题</button>
-      <ThemeContext.Provider value={{ theme, setTheme }}>
-        <ThemeContext value={{ theme, setTheme }}>
-          <Parent />
-        </ThemeContext>
-      </ThemeContext.Provider>
-      </div >
-    );
-  }
+      <h1>父组件</h1>
+      <input type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+      <table border={1} cellPadding={5} cellSpacing={0}>
+        <thead>
+          <tr>
+            <th>商品名称</th>
+            <th>商品价格</th>
+            <th>商品数量</th>
+          </tr>
+        </thead>
+        <tbody>
+          {goods.map(item => <tr key={item.id}>
+            <td>{item.name}</td>
+            <td>{item.price * item.count}</td>
+            <td>
+              <button onClick={() => handleAdd(item.id)}>+</button>
+              <span>{item.count}</span>
+              <button onClick={() => handleSub(item.id)}>-</button>
+            </td>
+          </tr>)}
+        </tbody>
+      </table>
+      <h2>总价：{total}</h2>
+    </div>
+  );
+}
 
-  export default App;
+export default App;
